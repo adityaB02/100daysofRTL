67bd3728098e06d12ccc21942e2dac5c1fcbb1de
adityaB02 <74639939+adityaB02@users.noreply.github.com>
2023-05-25 15:11:49 +0530

    Add files via upload

---
+++ DAY002/half_adder.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/25/2023 03:01:37 PM
+// Design Name: 
+// Module Name: half_adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module half_adder(input logic input_0,input_1,
+    output logic out);    
+    assign out = input_0 + input_1;   
+endmodule
